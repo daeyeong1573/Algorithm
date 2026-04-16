d9c91579aec36922960653805a8a76b52b796626
김대영 <rlaeodud9983@naver.com>
2026-04-17 00:22:59 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 46.57 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    answer = [...str].map((s)=> s===s.toUpperCase() ? s.toLowerCase():s.toUpperCase());
+    console.log(answer.join(""));
+});

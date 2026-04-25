5db59c8a8b38fc442548e0df026a908d6f83e2ba
김대영 <rlaeodud9983@naver.com>
2026-04-25 23:10:26 +0900

    [level 0] Title: 수 조작하기 2, Time: 35.89 ms, Memory: 40.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,10 @@
+function solution(numLog) {
+    const ctl = {"1":"w", "-1":"s", "10":"d", "-10":"a"};
+    
+    let answer = "";
+    for (let i = 1; i < numLog.length; i++) {
+        const diff = numLog[i] - numLog[i-1];
+        answer += ctl[diff];
+    }
+    return answer;
+}
